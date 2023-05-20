1f1e70214a34e29ecb040b64a5d23b534faa86bc
Isaias Dominguez <isaiasdr21@gmail.com>
2023-05-19 23:13:46 -0400

    Update: added styles app and create home screen

---
+++ lib/config/menu/menu_items.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+
+class MenuItem {
+  final String title;
+  final String subTitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItem({
+    required this.icon,
+    required this.link,
+    required this.subTitle,
+    required this.title,
+  });
+}
+
+const appMenuItems = <MenuItem> [
+    MenuItem(
+      icon: Icons.smart_button_outlined, 
+      link: '/buttons', 
+      subTitle: 'Multiple buttons in Flutter', 
+      title: 'Buttons'
+    ),
+
+    MenuItem(
+      icon: Icons.credit_card, 
+      link: '/cards', 
+      subTitle: 'Styled containers', 
+      title: 'Cards'
+    ),
+  ];
